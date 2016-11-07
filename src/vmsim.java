783d74f5d17b993e4424062d635f64078527ed18
Joe Meszar <github@outk.st>
2016-11-06 21:16:30 -0500

    Implemented CLOCK page replacement algorithm

--- src/vmsim.java
+++ src/vmsim.java
@@ -1,3 +1,9 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author Joe Meszar
@@ -7,15 +13,59 @@
  */
 
 public class vmsim {
+    static int numFrames = -1;
+    static int refresh = -1;
+    static int tau = -1;
+    static String algorithm="", traceFile="";
+    
     public static void main(String[] args) {
-        int numFrames = 0;
-        int refresh = 0;
-        int tau = 0;
-        String algorithm, traceFile;
+        //getCommandLineArgs(args);
         
+        numFrames = 5;
+        algorithm = "clock";
+        traceFile = "testTrace.txt";
+        
+        ClockRAM clockRAM = new ClockRAM(numFrames);
+        
+        String address = null;
+        boolean isRead = true;
+        
+        BufferedReader reader = null;
+        try {
+            File file = new File(traceFile);
+            reader = new BufferedReader(new FileReader(file));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                address = line.substring(0, 8);
+                if (line.substring(9, 10).equals("R")) {
+                    // read
+                    clockRAM.read(address);
+                } else {
+                    // write
+                    clockRAM.write(address);
+                }
+                
+                System.out.println(clockRAM + "\n");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    
+    private static void getCommandLineArgs(String[] args) {
         try {
             for (int i=0; i < args.length; i++) {
-                System.out.println(String.format("args[%d]: %s", i, args[i]));
                 
                 switch (args[i]) {
                     case "-n": // get number of frames
@@ -25,31 +75,32 @@ public class vmsim {
                     case "-a": // get the algorithm type to use
                         switch (args[i+1]) {
                             case "aging":   // aging algorithm that approximates LRU with 8bit counter
-                            case "clock:":  // better implementation of the (FiFo) 2nd chance algorithm
+                            case "clock":   // better implementation of the (FiFo) 2nd chance algorithm
                             case "opt":     // optimal page replacement
                             case "work":    // TBA 
+                                algorithm = args[i+1];
                                 break;
                                 
                             default:
                                 throw new Exception("Invalid algorithm type specified");
                         }
+                        break;
                         
                     case "-r": // get refresh (aging)
                         refresh = Integer.parseInt(args[i+1]);
+                        break;
                         
                     case "-t": // get working set clock
                         tau = Integer.parseInt(args[i+1]);
+                        break;
                         
                     default: // bad input
                         traceFile = args[i];
                 }  
             }            
         } catch (Exception e) {
-            System.out.println(e.getMessage() + 
-                    "\n\n./vmsim –n <numframes> ‐a <opt|clock|aging|work> [‐r <refresh>] [‐t <tau>] <tracefile>\n");
+            System.out.println(e.getMessage());
+            System.out.println(String.format("\n\njava vmsim –n <numframes> ‐a <opt|clock|aging|work> [‐r <refresh>] [‐t <tau>] <tracefile>\n"));
         }
-        
-        String asdf;
-        asdf = "asdf";
     }
 }
